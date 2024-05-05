cafbba65a886064aa1e4416a2a8c8c95237f90d8
shubham-rananaware <mail.shubham.jr@gmail.com>
2024-05-05 16:08:06 +0530

    initia

---
+++ src/main/java/com/jbk/config/BeanConfig.java
@@ -0,0 +1,25 @@
+package com.jbk.config;
+
+import org.modelmapper.ModelMapper;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class BeanConfig {
+
+
+	@Bean
+	public ModelMapper mapper () {
+		return new ModelMapper();
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate()
+	{
+		return new RestTemplate();
+	}
+}
